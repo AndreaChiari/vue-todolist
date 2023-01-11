50abdb9aebcd1be25524857841b055645a810b7b
AndreaChiari <chiariandrea94@gmail.com>
2023-01-11 16:26:43 +0100

    update at milestone 1

--- list.js
+++ list.js
@@ -1,1 +1,51 @@
 console.log('vue ok', Vue)
+
+const app = Vue.createApp({
+    data() {
+        return{
+            newTask: '',
+            tasks :[
+                {task:'fare la spesa',
+                 done: false
+                },
+            
+                {task:'aggiornare il computer',
+                 done: false
+                },
+                
+                {task:'allenarmi',
+                 done: false
+                },
+            
+                {task:'cucinare',
+                 done: false
+                },
+            
+                {task:'riparare il mouse',
+                 done: false
+                },
+            ],
+            
+        
+        }},
+
+        methods:{
+
+            removeTask(index){
+                this.tasks.splice(index,1);
+            },
+
+            addTask(){
+                this.tasks.push({task: this.newTask,
+                                 done: true   });
+                
+            },
+
+            addLine(){
+                this.tasks.done === true;
+            }
+           
+        }});
+        
+    
+app.mount('#root')
